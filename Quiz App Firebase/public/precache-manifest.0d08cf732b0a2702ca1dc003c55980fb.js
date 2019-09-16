b238b9330c7b926379b7951d75d78512f48cb084
JS Rajput <jsmaherrajput.com>
2019-09-17 01:31:48 +0500

    react quiz app

---
+++ Quiz App Firebase/public/precache-manifest.0d08cf732b0a2702ca1dc003c55980fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a79b400259f19babd9c789afe2d3f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7c072b7e932fd229c66",
+    "url": "/static/css/main.00ba1759.chunk.css"
+  },
+  {
+    "revision": "8b17b3d2b004f65e447e",
+    "url": "/static/js/2.9c0a432a.chunk.js"
+  },
+  {
+    "revision": "f7c072b7e932fd229c66",
+    "url": "/static/js/main.a923b9d0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "beb699f4985ef2881bf464ef711a223a",
+    "url": "/static/media/menus.beb699f4.png"
+  },
+  {
+    "revision": "b5c713a1ce39ba1ff0ce1102c7b3af30",
+    "url": "/static/media/social-logo-social-network-facebook-fb-social-media-icon.b5c713a1.svg"
+  }
+]);
